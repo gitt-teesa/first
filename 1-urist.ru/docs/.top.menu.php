4ad2cdd7a5b0a75bcf8aefbbd736e1e62e465ae1
websitemastering <Loner@.(none)>
2015-04-03 11:38:45 +0300

    new

---
+++ 1-urist.ru/docs/.top.menu.php
@@ -0,0 +1,32 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"О компании", 
+		"/about/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Каталог готовых фирм", 
+		"/gotovye-firmy/katalog-firm/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Вопросы-ответы", 
+		"/voprosy-i-otvety/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Контакты", 
+		"/contacts/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
